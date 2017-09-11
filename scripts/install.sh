4f36c09da6f673117540e8530307ed05aa5f3bf4
Vikash Kothary <kothary.vikash@gmail.com>
2017-09-11 02:59:04 +0100

    Initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# install.sh
+
+if ! which ansible; then
+  sudo apt-get install -y software-properties-common
+  sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt-get -qq update
+  sudo apt-get install -y ansible;
+fi
+ansible-playbook -i "localhost," -c local provision.yml --ask-become-pass
