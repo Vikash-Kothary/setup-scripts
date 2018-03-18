622e9032ccbc192528900e82bb1546e016ba51a0
Vikash Kothary <kothary.vikash@gmail.com>
2018-03-18 13:50:12 +0000

    Add script to uninstall Android Studio completely

---
+++ scripts/uninstall-android-studio.sh
@@ -0,0 +1,7 @@
+rm ~/.local/share/applications/androidstudio.desktop
+rm -rf /opt/android-studio/
+rm -rf ~/.android/
+rm -rf ~/.AndroidStudio*/
+rm -rf ~/Android
+rm -rf /opt/android-studio-ide-143.2790544-linux.zip
+rm -rf ~/.gradle
